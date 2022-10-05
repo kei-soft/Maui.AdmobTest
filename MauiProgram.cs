4f7ae668388128e01cd2dd6f080ebf1618267191
준 강 <junijuniya@naver.com>
2022-10-05 23:49:04 +0900

    초기 커밋.

---
+++ MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using Plugin.MauiMTAdmob;
+
+namespace Maui.AdmobTest
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiMTAdmob() // 추가
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            return builder.Build();
+        }
+    }
+}
